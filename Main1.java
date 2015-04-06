297bf4f02e10c1f487738bc0e04b31860028155f
Long Long <longlong2010@gmail.com>
2015-04-06 18:35:58 +0800

    save memory

--- Main1.java
+++ Main1.java
@@ -52,8 +52,14 @@ public class Main1 {
 		int M = 100;
 
 		double[] params1 = {0.1, 0.5, 1};
-		double[] params2 = {0.01, 0.1, 1};
+		double[] params2 = {0.01, 0.1, 1, 10.0};
 
+		Matrix data;
+		Grids grids;
+		int row, col;
+
+		PrintWriter writer;
+		
 		for (int i = 0; i < params1.length; i++) {
 			for (int j = 0; j < params2.length; j++) {
 
@@ -66,12 +72,6 @@ public class Main1 {
 				double delta_x = Ix.getLength() / M;
 				double delta_t = sigma * delta_x * delta_x / nu;
 				int N = (int) (It.getLength() / delta_t);
-
-				Matrix data;
-				Grids grids;
-				int row, col;
-				
-				PrintWriter writer;
 
 				//FTCS
 				FTCS ftcs = new FTCS(N, M, f, a, b, Ix, It) {
@@ -88,7 +88,6 @@ public class Main1 {
 				ftcs.solve();
 				data = ftcs.getValues();
 				grids = ftcs.getGrids();
-				
 				row = data.getRowSize();
 				col = data.getColumnSize();
 
@@ -101,8 +100,21 @@ public class Main1 {
 				} catch (Exception ex) {
 					ex.printStackTrace();
 				}
+			}
+		}
 
+		for (int i = 0; i < params1.length; i++) {
+			for (int j = 0; j < params2.length; j++) {
 
+				double sigma = params1[i];
+
+				Interval Ix = new Interval(0, 1);		
+				Interval It = new Interval(0, params2[j]);
+
+				// x step, t step
+				double delta_x = Ix.getLength() / M;
+				double delta_t = sigma * delta_x * delta_x / nu;
+				int N = (int) (It.getLength() / delta_t);
 				//BTCS
 				BTCS btcs = new BTCS(N, M, f, a, b, Ix, It) {
 					@Override
@@ -118,9 +130,10 @@ public class Main1 {
 						}
 				};
 				btcs.solve();
-
 				data = btcs.getValues();
 				grids = btcs.getGrids();
+				row = data.getRowSize();
+				col = data.getColumnSize();
 				
 				try {	
 					writer = new PrintWriter(new File("btcs" + String.valueOf(i) + "-" + String.valueOf(j)));
@@ -131,7 +144,21 @@ public class Main1 {
 				} catch (Exception ex) {
 					ex.printStackTrace();
 				}
+			}
+		}
 
+		for (int i = 0; i < params1.length; i++) {
+			for (int j = 0; j < params2.length; j++) {
+
+				double sigma = params1[i];
+
+				Interval Ix = new Interval(0, 1);		
+				Interval It = new Interval(0, params2[j]);
+
+				// x step, t step
+				double delta_x = Ix.getLength() / M;
+				double delta_t = sigma * delta_x * delta_x / nu;
+				int N = (int) (It.getLength() / delta_t);
 				//CNCS
 				CNCS cncs = new CNCS(N, M, f, a, b, Ix, It) {
 					@Override
@@ -149,9 +176,10 @@ public class Main1 {
 						}
 				};
 				cncs.solve();
-
 				data = cncs.getValues();
 				grids = cncs.getGrids();
+				row = data.getRowSize();
+				col = data.getColumnSize();
 				
 				try {	
 					writer = new PrintWriter(new File("cncs" + String.valueOf(i) + "-" + String.valueOf(j)));

bfc64761aa7c5641599c21268df32a7cad508680
Long Long <longlong2010@gmail.com>
2015-04-22 21:07:52 +0800

    add underRelaxation

--- Main3.java
+++ Main3.java
@@ -104,5 +104,42 @@ public class Main3 {
 
 		//Gauss-Seidel
 
+		for (int k = 0; k < Peclets.length; k++) {
+			double peclet = Peclets[k];
+			double delta_x = peclet / Pe * Ix.getLength();
+			int n = (int) Math.ceil(L / delta_x) - 1;
+			
+			Matrix m = new Matrix(n, n);
+			Vector b = new Vector(n);
+
+			m.set(0, 0, 2);
+			m.set(0, 1, 0.5 * peclet - 1);
+
+			b.set(0, (0.5 * peclet  + 1) * phi_0);
+
+			for (int i = 1; i < n - 1; i++) {
+				m.set(i, i - 1, -0.5 * peclet - 1);
+				m.set(i, i, 2);
+				m.set(i, i + 1, 0.5 * peclet - 1);
+
+				b.set(i, 0);
+			}
+			
+			m.set(n - 1, n - 2, -0.5 * peclet - 1);
+			m.set(n - 1, n - 1, 2);
+			
+			b.set(n - 1, (-0.5 * peclet + 1) * phi_l);
+			
+			Vector v0 = new Vector(n);
+			v0.set(0, 1);
+			Vector v = LinearEquations.underRelaxationInterationSolve(m, b, v0, 0.1, 1e-25, 10000);
+
+			for (int i = 0; i < n; i++) {
+				double x = Ix.getLeft() + (i + 1) * delta_x;
+				double y = analytic.value(x);
+				System.out.printf("%e\t%e\t%e\n", x, v.get(i), y);
+			}
+			System.out.println();
+		}
 	}
 }
--- equation/LinearEquations.java
+++ equation/LinearEquations.java
@@ -58,6 +58,47 @@ public class LinearEquations {
 		return v1;
 	}
 
+	public static Vector gaussSeidelInterationSolve(Matrix m, Vector b, Vector v0, double eps, int max_step) {
+		int size = m.getRowSize();
+		Vector v1 = v0;
+		for (int i = 0; i < max_step; i++) {
+			Vector delta = new Vector(size);
+			for (int j = 0; j < size; j++) {
+				double s = 0;
+				for (int k = 0; k < size; k++) {
+					s += m.get(j, k) * v1.get(k);
+				}
+				delta.set(j, (b.get(j) - s) / m.get(j, j));
+				v1.set(j, v1.get(j) + delta.get(j));
+			}
+			if (delta.length() < eps) {
+				break;
+			}
+		}
+		return v1;
+	}
+
+	public static Vector underRelaxationInterationSolve(Matrix m, Vector b, Vector v0, double omiga, double eps, int max_step) {
+		int size = m.getRowSize();
+		Vector v1 = v0;
+		for (int i = 0; i < max_step; i++) {
+			Vector delta = new Vector(size);
+			for (int j = 0; j < size; j++) {
+				double s = 0;
+				for (int k = 0; k < size; k++) {
+					s += m.get(j, k) * v1.get(k);
+				}
+				delta.set(j, (b.get(j) - s) / m.get(j, j));
+				v1.set(j, omiga * (v1.get(j) + delta.get(j)) + (1 - omiga) * v0.get(j));
+			}
+			v0 = v1;
+			if (delta.length() < eps) {
+				break;
+			}
+		}
+		return v1;
+	}
+
 	public static void main(String[] args) {
 		TriDiagonalMatrix m = new TriDiagonalMatrix(5);
 		m.set(0, 0, 2);

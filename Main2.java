82a7b707b638e5d081b06b118617ceb704fb75c4
Long Long <longlong2010@gmail.com>
2015-05-05 18:19:19 +0800

    fix bug

--- Main2.java
+++ Main2.java
@@ -112,7 +112,7 @@ public class Main2 {
 				}
 				
 				private double formula(double k1, double k2, double k3) {
-					return k2 - c * (k3 - k1) / 2 + c * c * (k3 - 2 * k2 + k3) / 2;
+					return k2 - c * (k3 - k1) / 2 + c * c * (k3 - 2 * k2 + k1) / 2;
 				}
 
 				@Override
